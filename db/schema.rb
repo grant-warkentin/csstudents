9ba3b5b6aa1c654a91905752398b07e2b11cdd69
grant <17gwarkentin@gmail.com>
2023-12-17 23:03:28 +0000

    Removed bio from Users;

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_17_225923) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_17_230105) do
   create_table "profiles", force: :cascade do |t|
     t.text "bio"
     t.datetime "created_at", null: false
@@ -25,7 +25,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_17_225923) do
     t.datetime "remember_created_at"
     t.string "first_name"
     t.string "last_name"
-    t.string "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
