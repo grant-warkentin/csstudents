29d2fa95635416b5cff8940680decd5037a17ae3
grant <17gwarkentin@gmail.com>
2023-12-18 00:10:44 +0000

    Create profile for user upon user creation;

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one :profile, dependent: :destroy
+  after_create :create_profile
+
+  private
+  def create_profile
+    Profile.create(user_id: self.id, bio:nil)
+  end
 end
